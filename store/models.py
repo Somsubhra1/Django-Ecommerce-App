ce5a3ba298f8f9aa02c5b439e2abd8deec1241e0
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-08 12:35:09 +0530

    added image field to product

--- store/models.py
+++ store/models.py
@@ -33,7 +33,7 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=100, null=True)
-    # image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
